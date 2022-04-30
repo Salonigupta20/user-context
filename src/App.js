ad343bfec5165ced1e8e4df0fa60b64225865b3b
Saloni Gupta <saloni.18jiec023@jietjodhpur.ac.in>
2022-04-30 17:40:11 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 import ComA from "./ComA"
 import ComC from "./ComC";
 const FirstName= createContext();
 const LastName= createContext();
+const Fullname= createContext();
 
 const App = () => {
+  const[name,setName]= useState();
+  const fullNameObj = { 
+    fname: name,
+    setFname: setName
+}
+
   return(
     <>
     <FirstName.Provider value={'Saloni'}>
     <LastName.Provider value={'Gupta'}>
+    <Fullname.Provider value={fullNameObj}>
   <ComA/>
+  </Fullname.Provider>
   </LastName.Provider>
   </FirstName.Provider>
   </>
@@ -17,4 +26,6 @@ const App = () => {
 }; 
 
 export default App;
-export {FirstName, LastName};
+export {FirstName, LastName,Fullname};
+
+
--- src/ComC.jsx
+++ src/ComC.jsx
@@ -1,12 +1,29 @@
-import React, { useContext } from "react";
-import { FirstName, LastName } from "./App";
+import React, { useContext,useReducer, useState  } from "react";
+import { FirstName, LastName, Fullname} from "./App";
 
 const ComC = () => {
     const fname= useContext(FirstName);
     const lname=useContext(LastName);
+    const fnameContext=useContext(Fullname);
+
+   
+    const InputEvent= (event)=>{
+        console.log(event.target.value);
+        fnameContext.setFname(event.target.value);
+    }
     return(
-        <h1>Hello this is {fname} {lname}</h1>
-    );
+        <>
+        <h1>Hello this is {fname} {lname} {fnameContext.fname}</h1>
+        <input 
+        type="text"
+        placeholder="Enter your name"
+        onChange={InputEvent}
+        value={fnameContext.fname}
+        />
+        <button onClick={InputEvent}>click me</button>
+    </>
+     );
     };
+
     export default ComC; 
     
